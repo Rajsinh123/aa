2a9349704a0033b7b71d063489d3c368ca7e7526
Rajsinh123 <rajsinhmandawale15@gmail.com>
2024-06-01 11:24:36 +0530

    Inital Commit

---
+++ Abstractionn/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstractionn
+{
+    abstract class Animal
+    {
+        public abstract void animalsound();
+    
+    public void sleep()
+    {
+        Console.WriteLine("ZZZZZZZZZZZZZ");
+    }
+    }
+    class Pig : Animal
+    {
+        public override void animalsound()
+        {
+            Console.WriteLine("Pig says+wee weee");
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pig mypig = new Pig();
+            mypig.animalsound();
+            mypig.sleep();
+            Console.ReadLine();
+        }
+    }
+}
